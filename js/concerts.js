b364ede5aad4f6bb459cfcc96614bedf8fae0343
joanferrando <44470063+joanferrando@users.noreply.github.com>
2026-06-23 18:01:09 +0200

    Update concerts.js

--- js/concerts.js
+++ js/concerts.js
@@ -1,1 +1,79 @@
+fetch('data/concerts.json')
+    .then(response => response.json())
+    .then(concerts => {
 
+        concerts.sort((a,b) => {
+
+            return new Date(a.data) - new Date(b.data);
+
+        });
+
+        const list =
+        document.getElementById('concerts-list');
+
+        const proxims =
+        concerts.slice(0,5);
+
+        proxims.forEach(concert => {
+
+            const bloc = document.createElement('div');
+
+            bloc.classList.add('concert-item');
+
+            bloc.innerHTML = `
+
+                <div class="concert-date">
+
+                    ${formatDate(concert.data)}
+
+                </div>
+
+                <div class="concert-info">
+
+                    <h3>${concert.poble}</h3>
+
+                    <p>${concert.event}</p>
+
+                    <span>${concert.adreca}</span>
+
+                </div>
+
+                <div class="concert-ticket">
+
+                    ${
+                        concert.link
+
+                        ?
+
+                        `<a href="${concert.link}" target="_blank">
+                            Entrades
+                        </a>`
+
+                        :
+
+                        `<span>De franc</span>`
+                    }
+
+                </div>
+
+            `;
+
+            list.appendChild(bloc);
+
+        });
+
+    });
+
+function formatDate(data){
+
+    const date = new Date(data);
+
+    return date.toLocaleDateString(
+        'ca-ES',
+        {
+            day:'2-digit',
+            month:'short'
+        }
+    );
+
+}
